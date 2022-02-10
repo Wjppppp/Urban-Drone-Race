e272a1142d951e0270e5cf63c04d0a1f41c97159
Jiapan Wang <wjp1999119@gmail.com>
2022-02-10 23:49:39 +0100

    update cities and drones

--- Urban Drone Race Test/Assets/Scripts/BladeRotation.cs
+++ Urban Drone Race Test/Assets/Scripts/BladeRotation.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class BladeRotation : MonoBehaviour
 {
     public float speed = 5000f;
-
+    public string dir = "z";
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        switch (dir)
+        {
+            case "z":
+                this.transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+                break;
+            case "x":
+                this.transform.Rotate(Vector3.right * Time.deltaTime * speed);
+                break;
+            case "y":
+                this.transform.Rotate(Vector3.up * Time.deltaTime * speed);
+                break;
+            default:
+                this.transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+                break;
+        }
+        
     }
 }
